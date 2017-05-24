2634e360b8a8313aaeac94853ce842de02ec4756
korrybass <korry@penheadcreative.com>
2017-05-24 15:15:41 -0400

    added files and updated socket on client and server

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const PORT = process.env.PORT || 4444;
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + "/public"));
+io.on('connection', () => {
+  console.log('user connected via socket.io');
+})
 
 http.listen(PORT, () => {
-  console.log('server started!');
+  console.log('server started...hot loaded!!');
 });
